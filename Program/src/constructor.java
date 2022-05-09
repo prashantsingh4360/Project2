d55aef78c010dc2c94b1a3b37d9cb42944790fa5
prashantsingh4360 <prashant.singh4360@gmail.com>
2022-05-09 09:48:32 +0530

    Program project created

---
+++ Program/src/constructor.java
@@ -0,0 +1,16 @@
+
+public class constructor {
+	
+	public constructor ()
+	{
+		int x=5;
+		System.out.println(x); //Widening
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		constructor c=new constructor();
+
+	}
+
+}
---
+++ Program/src/sample.java
@@ -0,0 +1,15 @@
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey");
+		int x=6;
+		System.out.println(x); //Widening
+		float y=x;
+		System.out.println(y);
+		int z=(int)y;
+		System.out.println(z); //dfsfsd
+	}
+
+}
